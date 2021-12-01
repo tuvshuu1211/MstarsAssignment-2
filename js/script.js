26744ae50ce8eb2ee32285b840237c4b4a40f045
tuvshuu1211 <inktwin@Enkhtuvshins-MacBook-Pro.local>
2021-12-01 17:11:39 +0800

    update

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function scrollDetect(e){
 
     if(st > window.innerHeight / 1.5){
         navbar.classList.add('fixed-top')
+        // navbar.className = 'fixed-top'
     }else{
         navbar.classList.remove('fixed-top')
     }
@@ -23,19 +24,19 @@ window.addEventListener('scroll', scrollDetect)
 const playButton = document.querySelector('.btn-video-player');
 
 // Create a modal with a single line of JavaScript: https://getbootstrap.com/docs/5.1/components/modal/#via-javascript
-// Define videoModal then use Bootstrap modal Object request
-let videoModal = new bootstrap.Modal(document.getElementById('finsetModal'), {
+// Define finsetModal then use Bootstrap modal Object request
+let finsetModal = new bootstrap.Modal(document.getElementById('finsetModal'), {
     keyboard: false
   })
 
 // Create Click Event Listener on Play button variable (you already created above)
-// add videoModal.show() on callback function
-// Select '.modal-body' class from videoModal variable then use innerHTML method and add Youtube embed code
+// add finsetModal.show() on callback function
+// Select '.modal-body' class from finsetModal variable then use innerHTML method and add Youtube embed code
 
 playButton.addEventListener('click', function(){
-    videoModal.show();
-    // console.log(videoModal)
-    videoModal._element.querySelector('.modal-body').innerHTML = `<iframe width="800" height="600" src="https://www.youtube.com/embed/7tz4Ya6gzG4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    finsetModal.show();
+    // console.log(finsetModal)
+    finsetModal._element.querySelector('.modal-body').innerHTML = `<iframe width="100%" height="600" src="https://www.youtube.com/embed/7tz4Ya6gzG4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 })
 
 // Create an XMLHttpRequest object
@@ -43,11 +44,12 @@ playButton.addEventListener('click', function(){
 // Open a GET request and use data from ../data/company_intro.json
 // Send the request
 
-let ajaxCompanyIntro = new XMLHttpRequest();
-ajaxCompanyIntro.onreadystatechange = function() {
-    if(ajaxCompanyIntro.readyState === 4){
-        if(ajaxCompanyIntro.status === 200){
-            let intros = JSON.parse(ajaxCompanyIntro.responseText);
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4){
+        if(xhr.status === 200){
+            // console.log( JSON.parse(xhr.responseText))
+            let intros = JSON.parse(xhr.responseText);
             let introDatas = intros.data
             let introMsg = intros.message
             const introPost = document.querySelector('#company-intro-posts .row');
@@ -65,12 +67,12 @@ ajaxCompanyIntro.onreadystatechange = function() {
                  `
             }
         } else {
-            alert(ajaxCompanyIntro.statusText)
+            alert(xhr.statusText)
         }
     }
 };
-ajaxCompanyIntro.open('GET', '../data/company_intro.json');
-ajaxCompanyIntro.send();
+xhr.open('GET', '../data/company_intro.json');
+xhr.send();
 
 
 /*  Add subscription email action. When subscription POST request is successful, 
@@ -85,19 +87,35 @@ buttonSubscribe.addEventListener('click', () => {
     const URL = 'http://52.221.191.153/subscribe/subscription/create';
     const submitValue = document.getElementById('finsweetInputEmail1').value;
     const requestOptions = {
-        method : "POST",
-        header : {'Content-Type': 'application/json'},
-        body: {
-            "email": submitValue
-        }
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: submitValue})
     }
+
+    
+fetch(URL, requestOptions).then(res => res.json())
+  .then(res => console.log(res));
+
     console.log(requestOptions);
     fetch(URL, requestOptions)
-    .then((response) => {return response.json()})
+    .then((response) => {
+        console.log(response.status)
+        if (response.status === 200){
+            return response.json()
+        }else{
+            throw new Error("Your submission is wrong");
+        }
+    })
     .then((data) => {
-        alert(data.message);
+        finsetModal.show();
+        finsetModal._element.querySelector('.modal-body').innerHTML = `<p class='text-success fs-1 p-5 text-center'>${data.message}!</p>`
     }).catch((error) => {
-        alert(error.message)}
+        finsetModal.show();
+        finsetModal._element.querySelector('.modal-body').innerHTML = `<p class='text-danger fs-1 p-5 text-center'>${error.message}!</p>`
+    }
     )
 }
 )
