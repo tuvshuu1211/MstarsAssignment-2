c60bffd59f6760ab87e011b7e6479c054489ec4e
Enkhtuvshin Bayarmagnai <enk@Enkhtuvshins-iMac.local>
2021-12-01 11:51:06 +0800

    update

--- js/script.js
+++ js/script.js
@@ -73,8 +73,15 @@ ajaxCompanyIntro.open('GET', '../data/company_intro.json');
 ajaxCompanyIntro.send();
 
 
-
-document.getElementById("subscribe").addEventListener('click', () => {
+/*  Add subscription email action. When subscription POST request is successful, 
+    change the email element and subscribe button into "Your subscription is successful" Text. 
+    POST request should be done by AJAX request. We need a POST request end point for subscription email. 
+*/
+// Define Subscribe by ID name
+buttonSubscribe = document.getElementById("subscribe");
+// Add CLICK Event Listener on the button
+// Then using Fetch method POST input value to http://52.221.191.153/subscribe/subscription/create 
+buttonSubscribe.addEventListener('click', () => {
     const URL = 'http://52.221.191.153/subscribe/subscription/create';
     const submitValue = document.getElementById('finsweetInputEmail1').value;
     const requestOptions = {
